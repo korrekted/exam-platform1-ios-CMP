83099b2cad1a7ef3e4cfc983244c4a9b02e2d5d9
akonst17 <akonst17@gmail.com>
2022-05-10 16:24:40 +0300

    build 1

---
+++ CMP/Classes/Misc/Appearance.swift
@@ -0,0 +1,25 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 252, green: 248, blue: 245)
+    
+    static let mainColor = UIColor(integralRed: 94, green: 19, blue: 82)
+    static let mainColorAlpha = UIColor(integralRed: 94, green: 19, blue: 82, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 245, green: 157, blue: 89)
+    
+    static let progress1Color = UIColor(integralRed: 94, green: 19, blue: 82)
+    static let progress2Color = UIColor(integralRed: 245, green: 157, blue: 89)
+    static let progress3Color = UIColor(integralRed: 223, green: 97, blue: 54)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+}
